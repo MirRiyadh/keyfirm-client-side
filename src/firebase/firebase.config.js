4224891547a7e995dd90f0f95fa061e6942cdeea
Riyadh <riyadh.ju@gmail.com>
2022-12-27 17:11:32 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZpS_5-jFTyIVfqDoGS0qUAYv8gycUPQQ",
+  authDomain: "keyfirm-d6931.firebaseapp.com",
+  projectId: "keyfirm-d6931",
+  storageBucket: "keyfirm-d6931.appspot.com",
+  messagingSenderId: "815808494846",
+  appId: "1:815808494846:web:a188fc2263b7f05e0bf9af",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/routes/routes.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main/Main";
+import ErrorPage from "../pages/errorPage/ErrorPage";
+import Home from "../pages/home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+  { path: "*", element: <ErrorPage></ErrorPage> },
+]);
